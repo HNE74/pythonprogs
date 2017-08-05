d0ec057d50dfa71ed2b4eb98312385c7ac51aa74
HNE74 <30633070+HNE74@users.noreply.github.com>
2017-08-05 19:48:53 +0200

    Pixelizer script using pillow API.

---
+++ Pixelizer/pixelizer.py
@@ -0,0 +1,102 @@
+'''
+Created on 05.08.2017
+
+@author: Heiko Nolte
+'''
+import argparse
+import numpy as np
+from PIL import Image, ImageDraw
+
+''' ******************************************************
+    *** Creates a raw pixeled output from input image. ***
+    ****************************************************** '''
+class Pixelizer:
+
+    input_file = None
+    output_file = None
+    output_scale = None
+    
+    image = None
+    img_width = None
+    img_height = None
+    pix_width = None
+    pix_height = None
+
+    def __init__(self, input_file, output_file, output_scale):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.output_scale = output_scale
+        
+    def execute(self):
+        self.image = Image.open(self.input_file)
+        
+        self.img_width, self.img_height = self.image.size[0], self.image.size[1]
+        self.pix_width = int(self.img_width / self.output_scale)
+        self.pix_height = int(self.img_height / self.output_scale)
+        print("Input image dims: %d x %d" % (self.img_width, self.img_height))
+        print("Output pixel dims: %d x %d" % (self.pix_width, self.pix_height))
+        
+        if self.pix_width == 0 or self.pix_height == 0:
+            print("Pixel width and pixel height must be at least 1, please decrease scale.")
+            return
+        else:
+            print("Start pixelizing image.")
+        self.pixelize()
+        print("Finished pixelizing image.")
+    
+    def pixelize(self):
+        x_steps = self.img_width / self.pix_width
+        y_steps = self.img_height / self.pix_height
+        x_pos = 0
+        y_pos = 0
+        new_image = Image.new('RGB', (self.img_width, self.img_height))
+        for y in range(y_steps):
+            for x in range(x_steps):
+                # Cut out subimage to be pixelized
+                subimage = self.image.crop((x_pos, y_pos, x_pos + self.pix_width, y_pos + self.pix_height))
+                # Generate pixel from subimage
+                pixel_image = self.get_pixel(subimage)
+                # Paste pixel to new image
+                new_image.paste(pixel_image, (x_pos, y_pos))
+                x_pos += self.pix_width
+            x_pos = 0
+            y_pos += self.pix_height
+        new_image.save(self.output_file)
+                
+    def get_pixel(self, image):
+        # Calculate avarage color
+        image_array = np.array(image)
+        color_sum = np.full((3), 0, dtype=int)
+        for y in range(self.pix_width):
+            for x in range(self.pix_height):
+                color_val = image_array[x, y]
+                color_sum = np.add(color_val, color_sum)                
+        color_avg = np.divide(color_sum, self.pix_width * self.pix_height)
+        
+        # Create new pixel image having avarage color
+        pixel_img = Image.new('RGB', (self.pix_width, self.pix_height))
+        draw = ImageDraw.Draw(pixel_img)
+        draw.rectangle(((0,0),(self.pix_width, self.pix_height)), fill=tuple(color_avg))
+        return pixel_img
+
+''' ******************************************************
+    *** Script parameters:                             ***
+    *** --file: Input file                             ***
+    *** --scale: Pixel creation ratio                  ***
+    *** --out: Output file                             ***            
+    ****************************************************** '''
+def main():
+    # create parser
+    descStr = "This program converts an image into ASCII art."
+    parser = argparse.ArgumentParser(description=descStr)
+    # add expected arguments
+    parser.add_argument('--file', dest='imgFile', required=True)
+    parser.add_argument('--scale', dest='scale', required=True)
+    parser.add_argument('--out', dest='outFile', required=True)
+    args = parser.parse_args() 
+     
+    pixelizer = Pixelizer(args.imgFile, args.outFile, int(args.scale))
+    pixelizer.execute()
+
+if __name__ == '__main__':
+    main()
